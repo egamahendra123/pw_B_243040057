65a80faf14202650bfcce41143914a5a0505e198
egamahendra123 <mahendraega8@gmail.com>
2025-02-17 18:09:59 +0700

    selesai mengerjakan tugas 1 pw

---
+++ kuliah/pertemuan2/latihan2.php
@@ -0,0 +1,13 @@
+<?php 
+    $nama = "ega mahendra";
+    $email = "mahendraega8@gmail.com";
+    $pekerjaan ="pelajar/mahasiswa";
+    $jurusan ="teknik informatika";
+    $universitas ="Universitas pasundan";
+
+    echo "<h1>$nama</h1>";
+    echo "<h4>$email</h4>";
+    echo "<p>$pekerjaan</p> ";
+    echo "<p>$jurusan</p> ";
+    echo "<p>$universitas</p> ";
+?>
---
+++ tugas/tugas1/tugas1b.php
@@ -0,0 +1,14 @@
+<?php
+$npm = 57 ; 
+
+echo "Aku adalah angka <b>$npm</b><br>";
+$npm *= 5;
+echo "Jika aku di kali 5, maka aku sekarang menjadi <b>$npm</b><br>";
+$npm /= 2;
+echo "jika aku di bagi 2, maka sekarang aku menjadi <b>$npm</b><br>";
+$npm += 75;
+echo "jika aku di tambah 75, maka sekarang aku menjadi <b>$npm</b><br>";
+$npm -= 20;
+echo "Jika aku di kurang 20 maka aku sekarang menjadi <b>$npm</b><br>";
+?>
+
